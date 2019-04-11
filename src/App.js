c17cc78657e3bb2afe8010677fd3f22b702892f6
teropaj <teropaj@gmail.com>
2019-04-11 08:35:56 +0300

    filter

--- src/App.js
+++ src/App.js
@@ -4,26 +4,7 @@ import './App.css';
 import Search from './components/Search'
 
 class App extends Component {
-  myFunction() {
-    console.log('jlsjfls')
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li.length; i++) {
-      a = li[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-      } else {
-        li[i].style.display = "none";
-      }
-    }
-  }
+ 
   
   
   render() {
